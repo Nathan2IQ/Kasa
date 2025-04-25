34d1a77b27b67ecdd209951810e5643037ef326f
Nathan2IQ <nathankent196@gmail.com>
2025-04-25 10:17:01 +0200

    Initialisation du Projet

---
+++ src/components/Test.jsx
@@ -0,0 +1,9 @@
+function TestComponent () {
+    return (
+        <div>
+            <h1 style={{ margin: '400px' }}>Hello World</h1>
+        </div>
+    )
+}
+
+export default TestComponent
--- src/pages/Home/main.jsx
+++ src/pages/Home/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../../styles/index.css'
-import App from '../../components/App.jsx'
+
+import Test from '../../components/Test.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Test />
   </StrictMode>,
 )
